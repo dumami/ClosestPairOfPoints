97f89b049c18c66df398fa44a485677cb109de6a
Maria Eduarda <dudamachadom@gmail.com>
2020-11-30 22:25:23 -0300

    update

--- src/ParPontosMaisProximos.java
+++ src/ParPontosMaisProximos.java
@@ -4,29 +4,52 @@ public class ParPontosMaisProximos {
 
 	static double menorDistancia = Integer.MAX_VALUE;
 	static Point p1 = null, p2 = null;
+	
+	static final int TOTALPONTOS = 50_000;
+	static final int MINPONTOS = 2;
+
+	static Random sorteio = new Random();
 
 	public static void main(String[] args) {
 
-		/*Point p11 = new Point(2, 3);
-		Point p22 = new Point(12, 30);
-		Point p33 = new Point(40, 50);
-		Point p44 = new Point(5, 1);
-		Point p55 = new Point(12, 10);
-		Point p66 = new Point(3, 4);
-		List<Point> list = new ArrayList<>();
-		list.add(p11);
-		list.add(p22);
-		list.add(p33);
-		list.add(p44);
-		list.add(p55);
-		list.add(p66); */
-		
-		
+		/*
+		 * Point p11 = new Point(2, 3); 
+		 * Point p22 = new Point(12, 30); 
+		 * Point p33 = new Point(40, 50); 
+		 * Point p44 = new Point(5, 1); 
+		 * Point p55 = new Point(12, 10);
+		 * Point p66 = new Point(3, 4); 
+		 * List<Point> list = new ArrayList<>();
+		 * list.add(p11); 
+		 * list.add(p22); 
+		 * list.add(p33); 
+		 * list.add(p44); 
+		 * list.add(p55);
+		 * list.add(p66);
+		 */
+
+		List<Point> list = geraPontos(TOTALPONTOS);
+
 		menorDistancia(list);
-		System.out.println("The closest pair of points are (" + p1.x + "," + p1.y + ") (" + p2.x + "," + p2.y
-				+ ") and the distance between them is " + menorDistancia);
+		System.out.println("O par de pontos mais próximo é: (" + p1.x + "," + p1.y + ") (" + p2.x + "," + p2.y
+				+ ") e a distância entre eles é " + menorDistancia);
 	}
 
+	public static List<Point> geraPontos(int quantPontos) {
+		List<Point> pontos = new ArrayList<Point>(quantPontos);
+		for (int i = 0; i < quantPontos; i++) {
+			pontos.add(new Point(sorteio.nextInt(TOTALPONTOS), sorteio.nextInt(TOTALPONTOS)));
+		}
+
+		Point x = new Point(1000, 1000);
+		Point y = new Point(1001, 1001);
+		pontos.set(TOTALPONTOS / 2, x);
+		pontos.set(TOTALPONTOS / 2 + 1, y);
+
+		return pontos;
+	}
+
+	// Classe Point
 	public static class Point {
 		private int x;
 		private int y;
@@ -47,7 +70,8 @@ public class ParPontosMaisProximos {
 		int n = Pontos.size();
 
 		Point[] pontosPorX = new Point[n];
-
+		
+		
 		for (int i = 0; i < n; i++) {
 			pontosPorX[i] = Pontos.get(i);
 		}
@@ -93,11 +117,14 @@ public class ParPontosMaisProximos {
 		if (fim <= comeco)
 			return Double.POSITIVE_INFINITY;
 
+		//encontrar posição do meio
 		int meio = comeco + (fim - comeco) / 2;
+		
+		//pegar o ponto (valor) na posição do meio
 		Point mediana = pontosPorX[meio];
 
 		// Calcula o par de pontos mais próximos com ambos os pontos de extremidades no
-		// sub array da esquerda ou ambos no sub array da direita.
+		// subarray da esquerda ou ambos no sub array da direita.
 
 		double delta1 = maisProximos(pontosPorX, pontosPorY, aux, comeco, meio);
 		double delta2 = maisProximos(pontosPorX, pontosPorY, aux, meio + 1, fim);
@@ -117,6 +144,7 @@ public class ParPontosMaisProximos {
 
 		// compara cada ponto com seus vizinho fixados na coordenada Y mais prox que o
 		// delta
+		
 		for (int i = 0; i < m; i++) {
 			for (int j = i + 1; (j < m) && (aux[j].y - aux[i].y < delta); j++) {
 				double distance = getDistance(aux[i], aux[j]);
@@ -134,6 +162,7 @@ public class ParPontosMaisProximos {
 	}
 
 	private static void merge(Point[] a, Point[] aux, int comeco, int meio, int fim) {
+		
 		// copiar para o AUX
 		for (int i = comeco; i <= fim; i++) {
 			aux[i] = a[i];
